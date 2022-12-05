d6ef325e2c2964d2038f71976a3d84c8472a6e5d
Djam1977 <117649014+Djam1977@users.noreply.github.com>
2022-12-05 16:18:47 +0100

    second cow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const info = require("./information")
 let cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : "I'm a moooodule",
+    text : `I'm ${info.student.myName} "from" ${info.student.campus}`,
     e : "oO",
     T : "U "
 }));
